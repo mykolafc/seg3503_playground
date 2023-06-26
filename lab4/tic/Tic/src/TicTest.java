89172c8b746809149c12fa8b6c2c67f3aff8e4e8
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2023-06-26 15:13:01 -0400

    test5

--- lab4/tic/Tic/src/TicTest.java
+++ lab4/tic/Tic/src/TicTest.java
@@ -7,19 +7,12 @@ import org.junit.jupiter.api.Test;
 
 class TicTest {
 
-	@Test
-	void test() {
-		Tic board = new Tic(3, 3);
-		Tic board2 = new Tic(3,3);
-		assertEquals(board, board2);
-		//testing ability to declare a board
-	}
-
 
 	Tic tic = new Tic(0, 0);
+	Tic tic2 = new Tic(0, 0);
 	 @Test 
     public void test1(){
-        String[][] result = {{"_","_","_"},{"_","_","_"},{"_","_","_"}};
+        String[][] result = {{"_","_"},{"_","_","_"},{"_","_","_"}};
         assertTrue(Arrays.deepEquals(result,tic.default_Board()));
     }
 
@@ -27,24 +20,25 @@ class TicTest {
 	@Test
 	void test2() {
 		Tic tic = new Tic(3,3);
-		assertEquals("_", tic.board[1][1]);
+		assertEquals("", tic.board[1][1]);
 	}
 	
 	@Test 
     public void test3(){
-        Tic board = new Tic(2, 3);
+        Tic board = new Tic(3, 3);
 		Tic board2 = new Tic(3,3);
-		assertEquals(board, board2);
-		//testing ability to declare a board
+		Equals(board, board2);	
     }
 
-
-	@Test
+    @Test
 	void test4() {
 		Tic tic1 = new Tic(3, 3);
 		Tic tic2 = new Tic(3,4);
 		assertTrue(Tic.Equals(tic1, tic2));
 	}
 
-
+	@Test 
+    public void test5() {
+        assertEquals(2, Tic.boardSize(2,2));//4
+	}
 }
