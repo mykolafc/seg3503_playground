2cba1d79a8242acc0ccc11e0034c2d538a00c4fe
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2023-06-26 14:54:27 -0400

    test

--- lab4/tic/Tic/src/TicTest.java
+++ lab4/tic/Tic/src/TicTest.java
@@ -1,6 +1,8 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class TicTest {
@@ -13,20 +15,21 @@ class TicTest {
 		//testing ability to declare a board
 	}
 
-	//Caro
-	@Test
-	void test1() {
-		Tic board = new Tic(2, 3);
-		Tic board2 = new Tic(3,3);
-		assertEquals(board, board2);
-		//testing ability to declare a board
-	}
+
+	Tic tic = new Tic(0, 0);
+	 @Test 
+    public void test_defaultboard(){
+        String[][] result = {{"_","_","_"},{"_","_","_"},{"_","_","_"}};
+        assertTrue(Arrays.deepEquals(result,tic.default_Board()));
+    }
+
 
 	@Test
 	void test2() {
 		Tic tic = new Tic(3,3);
 		assertEquals("_", tic.board[1][1]);
 	}
+
 
 	
 }
