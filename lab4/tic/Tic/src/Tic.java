a9ef85c8890fc09cff4d0736abbf108f86b38743
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2023-06-26 16:58:58 -0400

    Final

--- lab4/tic/Tic/src/Tic.java
+++ lab4/tic/Tic/src/Tic.java
@@ -98,7 +98,7 @@ public class Tic {
 		}
 		return true;
 	}
-
+	
 
 
 }
--- lab4/tic/Tic/src/TicTest.java
+++ lab4/tic/Tic/src/TicTest.java
@@ -12,7 +12,7 @@ class TicTest {
 	Tic tic2 = new Tic(0, 0);
 	 @Test 
     public void test1(){
-        String[][] result = {{"_","_"},{"_","_","_"},{"_","_","_"}};
+        String[][] result = {{"_","_","_"},{"_","_","_"},{"_","_","_"}};
         assertTrue(Arrays.deepEquals(result,tic.default_Board()));
     }
 
@@ -20,48 +20,48 @@ class TicTest {
 	@Test
 	void test2() {
 		Tic tic = new Tic(3,3);
-		assertEquals("", tic.board[1][1]);
+		assertEquals("_", tic.board[1][1]);
 	}
 	
 	@Test 
     public void test3(){
        Tic board=new Tic(3,3);
 		board.placeO(1,1);
-		assertEquals(board.board[1][1],"x");	
+		assertEquals(board.board[1][1],"o");	
     }
 
     @Test
 	void test4() {
 		Tic tic1 = new Tic(3, 3);
-		Tic tic2 = new Tic(3,4);
+		Tic tic2 = new Tic(3,3);
 		assertTrue(Tic.Equals(tic1, tic2));
 	}
 
 	@Test 
     public void test5() {
-        assertEquals(2, Tic.boardSize(2,2));//4
+        assertEquals(4, Tic.boardSize(2,2));
 	}
 
 	@Test
 	public void test6() {
 		Tic tic = new Tic(3, 3);
 		for(int i=0; i<3; i++){
-			tic.board[0][i] = "X";
+			tic.board[0][i] = "o";
 		}
-		assertTrue(tic.fullRow(1));
+		assertTrue(tic.fullRow(2));
 	}
 
 	@Test
     public void test7() {
         String[][] result = {{"_","_","_"},{"_","_","_"}};
-        assertTrue(Arrays.deepEquals(result,tic.emptyboard(4,3)));//2
+        assertTrue(Arrays.deepEquals(result,tic.emptyboard(2,3)));
     }
 
 	@Test
 	public void test8() {
 		Tic tic = new Tic(3, 3);
 		for(int i=0; i<3; i++){
-			tic.board[i][0] = "O";
+			tic.board[i][0] = "X";
 		}
 		assertTrue(tic.fullCol(1));
 	}
@@ -70,7 +70,7 @@ class TicTest {
 	public void test9(){
 		Tic board=new Tic(3,3);
 		board.placeX(1,1);
-		assertEquals(board.board[1][1],"o");
+		assertEquals(board.board[1][1],"x");
 	}
 
 	@Test
