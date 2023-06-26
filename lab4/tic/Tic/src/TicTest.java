52059f7ede782b593cf65167ff13a6f3ffa83b1a
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2023-06-26 15:01:44 -0400

    test3

--- lab4/tic/Tic/src/TicTest.java
+++ lab4/tic/Tic/src/TicTest.java
@@ -18,7 +18,7 @@ class TicTest {
 
 	Tic tic = new Tic(0, 0);
 	 @Test 
-    public void test_defaultboard(){
+    public void test1(){
         String[][] result = {{"_","_","_"},{"_","_","_"},{"_","_","_"}};
         assertTrue(Arrays.deepEquals(result,tic.default_Board()));
     }
@@ -29,6 +29,15 @@ class TicTest {
 		Tic tic = new Tic(3,3);
 		assertEquals("_", tic.board[1][1]);
 	}
+	
+	@Test 
+    public void test3(){
+        Tic board = new Tic(2, 3);
+		Tic board2 = new Tic(3,3);
+		assertEquals(board, board2);
+		//testing ability to declare a board
+    }
+
 
 	@Test
 	void test4() {
